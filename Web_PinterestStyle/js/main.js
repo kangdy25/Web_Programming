e9cff98c884f3b677ef7740dc1785647ac8b3d27
Kangdy <canfrog@naver.com>
2021-11-19 16:25:10 +0900

    Complete 7th Web Project

--- Web_PinterestStyle/js/main.js
+++ Web_PinterestStyle/js/main.js
@@ -1,7 +1,67 @@
+// 절차 지향 방식
 window.addEventListener("load", ()=>{
     const grid = new Isotope("section", {
         itemSelector: "article",
         columnWidth: "article",
         transitionDuration: "0.5s"
     });
+
+    const btns = document.querySelectorAll("main ul li");
+
+    for(let el of btns){
+        el.addEventListener("click", e=>{
+            e.preventDefault();
+
+            const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+            grid.arrange({
+                filter : sort
+            });
+
+            for(let el of btns){
+                el.classList.remove(on);
+            }
+            e.currentTarget.classList.add("on");
+        })
+    }
 });
+
+// 객체 지향 방식
+// const frame = "section";
+// const box = "article";
+// const speed = "0.5s";
+// const activeClass = "on";
+// const btn = document.querySelectorAll("main ul li");
+// let grid;
+
+// window.addEventListener("load", ()=>{
+//     Init();
+//     filter(btn);
+// });
+
+// function init(){
+//     grid = new Isotope(frame, {
+//         itemSelector: box,
+//         columnWidth: box,
+//         transitionDuration: speed
+//     });
+// }
+
+// function filter(arr){
+//     for(let el of arr){
+//         el.addEventListener("click", e=>{
+//             e.preventDefault();
+
+//             const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+//             grid.arrange({
+//                 filter : sort
+//             });
+
+//             for(let el of arr){
+//                 el.classList.remove(activeClass);
+//             }
+//             e.currentTarget.classList.add(activeClass)
+//         })
+//     }
+// }
